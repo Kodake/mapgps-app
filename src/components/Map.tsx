7c01aa1c608229081a85d7965a701edf6c5643ed
Kodake <kodake@hotmail.es>
2022-01-31 00:29:25 -0400

    Adding polylines to our map route

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,24 +1,57 @@
-import React, { useRef } from 'react';
-import MapView from 'react-native-maps';
+import React, { useEffect, useRef, useState } from 'react';
+import MapView, { Polyline } from 'react-native-maps';
 import { useLocation } from '../hooks/useLocation';
 import LoadingScreen from '../screens/LoadingScreen';
 import FabIcon from './FabIcon';
 
 const Map = () => {
-
-    const { hasLocation, initialPosition, getCurrentLocation } = useLocation();
+    const [showPolyline, setShowPolyline] = useState(true);
+    const { hasLocation,
+        initialPosition,
+        getCurrentLocation,
+        followUserLocation,
+        userLocation,
+        stopFollowUserLocation,
+        routeLines } = useLocation();
 
     const mapViewRef = useRef<MapView>();
+    const following = useRef<boolean>(true);
+
+    useEffect(() => {
+        followUserLocation();
+
+        return () => {
+            stopFollowUserLocation();
+        }
+    }, []);
+
+    useEffect(() => {
+
+        if (!following.current) return;
+
+        const location = userLocation;
+        mapViewRef.current?.animateCamera({
+            center: {
+                latitude: location?.latitude!,
+                longitude: location?.longitude!
+            },
+            zoom: 15,
+        });
+    }, [userLocation]);
+
 
     const centerPosition = async () => {
         const location = await getCurrentLocation();
+
+        following.current = true;
+
         mapViewRef.current?.animateCamera({
             center: {
                 latitude: location.latitude,
                 longitude: location.longitude
             },
             zoom: 15,
-        })
+        });
     }
 
     if (!hasLocation) return <LoadingScreen />;
@@ -40,8 +73,8 @@ const Map = () => {
                 zoomControlEnabled
                 maxZoomLevel={30}
                 minZoomLevel={15}
+                onTouchStart={() => following.current = false}
             >
-
                 {/* <Marker
                     image={require('../assets/custom-marker.png')}
                     coordinate={{
@@ -51,6 +84,17 @@ const Map = () => {
                     title='Title 1'
                     description='This is a Title 1'
                 /> */}
+
+                {
+                    showPolyline && (
+                        <Polyline 
+                            coordinates={ routeLines }
+                            strokeColor="black"
+                            strokeWidth={ 3 }
+                        />
+                    )
+                }
+
             </MapView>
 
             <FabIcon
@@ -59,7 +103,17 @@ const Map = () => {
                 style={{
                     position: 'absolute',
                     bottom: 20,
-                    right: 20
+                    right: 65
+                }}
+            />
+
+            <FabIcon
+                iconName='brush-outline'
+                onPress={() => setShowPolyline(!showPolyline)}
+                style={{
+                    position: 'absolute',
+                    bottom: 80,
+                    right: 65
                 }}
             />
         </>
--- src/hooks/useLocation.tsx
+++ src/hooks/useLocation.tsx
@@ -1,17 +1,33 @@
 import Geolocation from '@react-native-community/geolocation';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { Location } from '../interfaces/appInterfaces';
 
 
 export const useLocation = () => {
 
     const [hasLocation, setHasLocation] = useState(false);
+    const [routeLines, setRouteLines] = useState<Location[]>([]);
     const [initialPosition, setInitialPosition] = useState<Location>();
+    const [userLocation, setUserLocation] = useState<Location>();
+
+    const watchId = useRef<number>();
+    const isMounted = useRef(true);
+
+    useEffect(() => {
+        isMounted.current = true;
+        return () => {
+            isMounted.current = false;
+        };
+    }, []);
+
 
     useEffect(() => {
         getCurrentLocation()
             .then(location => {
+                if (!isMounted.current) return;
                 setInitialPosition(location);
+                setUserLocation(location);
+                setRouteLines(_routes => [...routeLines, location]);
                 setHasLocation(true);
             });
     }, []);
@@ -30,9 +46,35 @@ export const useLocation = () => {
         });
     }
 
+    const followUserLocation = () => {
+        watchId.current = Geolocation.watchPosition(
+            ({ coords }) => {
+                if (!isMounted.current) return;
+                const location: Location = {
+                    latitude: coords.latitude,
+                    longitude: coords.longitude
+                }
+
+                setUserLocation(location);
+                setRouteLines(_routes => [...routeLines, location]);
+            },
+            (err) => console.log(err), { enableHighAccuracy: true, distanceFilter: 10 }
+        );
+    }
+
+    const stopFollowUserLocation = () => {
+        if (watchId.current) {
+            Geolocation.clearWatch(watchId.current);
+        }
+    }
+
     return {
         hasLocation,
         initialPosition,
-        getCurrentLocation
+        getCurrentLocation,
+        followUserLocation,
+        stopFollowUserLocation,
+        userLocation,
+        routeLines
     }
 };
